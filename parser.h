06cc6b5f671ed38372498d3e40d52169f7feeec7
James Addison <addison.j@etoncollege.org.uk>
2022-10-18 15:18:23 +0100

    why didn't that commit properly :(

--- parser.h
+++ parser.h
@@ -11,6 +11,39 @@ typedef struct {
 
 DECL_VEC(ParseError, ParseErrList)
 
+typedef struct Expression Expression;
+
+typedef struct {
+
+} UnaryExpr;
+
+typedef struct {
+
+} BinaryExpr;
+
+typedef struct {
+
+} CallExpr;
+
+typedef struct {
+
+} SuperExpr;
+
+typedef Expression GroupingExpr;
+typedef Token PrimaryExpr;
+
+struct Expression {
+    enum { Expr_Unary, Expr_Binary, Expr_Call, Expr_Super, Expr_Grouping, Expr_Literal } tag;
+    union {
+        UnaryExpr unary;
+        BinaryExpr binary;
+        CallExpr call;
+        SuperExpr super;
+        GroupingExpr grouping;
+        PrimaryExpr primary;
+    };
+};
+
 typedef struct {
 
 } ExprStmt;
@@ -40,7 +73,16 @@ typedef struct {
 } SwitchStmt;
 
 typedef struct {
-    enum { Stmt_Expr, Stmt_Global, Stmt_For, Stmt_ } tag;
+    enum { Stmt_Expr, Stmt_Global, Stmt_For, Stmt_While, Stmt_Do, Stmt_If, Stmt_Switch } tag;
+    union {
+        ExprStmt expr;
+        GlobalStmt global;
+        ForStmt for_;
+        WhileStmt while_;
+        DoStmt do_;
+        IfStmt if_;
+        SwitchStmt switch_;
+    };
 } Statement;
 
 typedef struct {
